d13f7d8f96d6d58974f40147a5ea5665e53cc8e3
Velasquez, David (dv365y) <dv365y@att.com>
2017-11-14 14:22:22 -0800

    [111417] Adds pathfinder service to main controller

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -300,6 +300,49 @@ var checkSubs = function(obj, premium, sub) {
 (function(angular) {
     'use strict';
     angular.module('entertainment')
+        .service('pathFinder', ['URLS', '$location', '$http',
+            function (URLS, $location, $http) {
+                this.getApiNet = function() {
+                    var location = $location.host(),
+                        network = function(network) {
+                            var basePath;
+                            switch (network) {
+                                case 'intra':
+                                    basePath = URLS.API_INTRA;
+                                    break;
+
+                                case 'stage':
+                                    basePath = URLS.API_STAGE;
+                                    break;
+
+                                case 'extra':
+                                    basePath = URLS.API_EXTRA;
+                                    break;
+
+                                case 'test':
+                                    basePath = URLS.API_DEV;
+                                    break;
+                            }
+                            return basePath;
+                        };
+                    if (location == 'vwecda05.testla.testfrd.directv.com' || location == 'localhost') {
+                        return $http.jsonp('https://intra3.web.att.com/toolupdater/Web/api/values/1?callback=JSON_CALLBACK').then(function successTest(response){
+                            return network('test');
+                        });
+                    } else if (location == 'zlp09097.vci.att.com') {
+                        return $http.jsonp('https://intra3.web.att.com/toolupdater/Web/api/values/1?callback=JSON_CALLBACK').then(function successTest(response){
+                            return network('stage');
+                        });
+                    } else {
+                        return $http.jsonp('https://intra3.web.att.com/toolupdater/Web/api/values/1?callback=JSON_CALLBACK').then(function successTest(response) {
+                            return network('intra');
+                        }, function errorTest(response) {
+                            return network('extra');
+                        });
+                    }
+                }
+            }
+        ])
         .controller('RsnCtrl', ['$scope', 'DTOptionsBuilder', 'DTColumnBuilder', '$filter',
             function ($scope, DTOptionsBuilder, DTColumnBuilder, $filter) {
                 var ajaxObj = function() {
