77a589b3c3cfb894c4570e2a2c4022679e468723
Velasquez, David (dv365y) <dv365y@att.com>
2017-12-20 13:27:06 -0800

    [122017] Fixes issue with iis server rejecting request

--- config/config.js
+++ config/config.js
@@ -4,11 +4,8 @@
         .config(['$routeProvider', '$locationProvider', '$httpProvider',
             function($routeProvider, $locationProvider, $httpProvider) {
                 // Disable IE ajax request caching
-                if (!$httpProvider.defaults.headers.get) {
-                    $httpProvider.defaults.headers.get = {};
-                }
-
-                $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+                $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+                $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 
                 // Routing logic for calendar and premium views
                 // /:premName/:subName are variables, can be anything (hbo/overview or mtv/news) set by url
